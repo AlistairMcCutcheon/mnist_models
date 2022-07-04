8a0b18073bb2df43f9e460d93f0b12140072e4dc
Alistair McCutcheon <chessnutter@gmail.com>
2022-07-04 11:17:03 +1000

    Added convolution_network.py with ConvNet()

---
+++ src/convolutional_network.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+
+
+class ConvNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 20, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(20, 64, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Flatten(1, -1),
+            nn.Linear(64 * 5 * 5, 128),
+            nn.ReLU(),
+            nn.Linear(128, 10),
+        )
+
+    def forward(self, x):
+        return self.model(x)
--- src/dataloader.py
+++ src/dataloader.py
@@ -2,19 +2,24 @@ import numpy as np
 from matplotlib import pyplot as plt
 from torch.utils.data import DataLoader
 from dataset import DatasetMNIST
+import torchvision
+from convolutional_network import ConvNet
 
 image_size = 28
 data_path = "data/"
 dataset = DatasetMNIST(data_path, image_size)
 print(len(dataset))
 
-dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
 
 batch_images, batch_labels = next(iter(dataloader))
-print(batch_images.shape)
-print(batch_labels.shape)
-for image, label in zip(batch_images, batch_labels):
-    print(label)
-    image = np.asarray(image).squeeze()
-    plt.imshow(image)
-    plt.show()
+
+# image_grid = torchvision.utils.make_grid(batch_images, padding=2, pad_value=255)
+# plt.imshow(np.transpose(image_grid, (1, 2, 0)))
+# plt.show()
+
+network = ConvNet()
+out = network.forward(batch_images)
+print(type(out))
+print(out.shape)
+print(out)
